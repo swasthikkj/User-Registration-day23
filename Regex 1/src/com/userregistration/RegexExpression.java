3d75af732fbd53ea899fd608a0d22963408f73cd
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-04-20 09:00:37 +0530

    program

---
+++ Regex 1/src/com/userregistration/RegexExpression.java
@@ -0,0 +1,19 @@
+package com.userregistration;
+
+import java.util.Scanner;
+
+public class RegexExpression {
+	static String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+	public static void main(String[] args) {
+		Scanner  scan = new Scanner(System.in);
+		System.out.println("Enter the first name:");
+		String testName = scan.nextLine();
+		if(testName.matches(firstName)) {
+			System.out.println("This is valid first name");
+		}
+
+		else {
+			System.out.println("Not a valid name");
+		}
+	}
+}
